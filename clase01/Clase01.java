52556fe903ca97127ba6c061e15e8d38fc560367
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-03-19 23:18:17 -0300

    Add files via upload

---
+++ clase01/Clase01.java
@@ -0,0 +1,71 @@
+package clase01;
+/**
+ * Clase principal del proyecto. 
+ * Curso Java Clase 1.
+ * @author Carlos
+ */
+public class Clase01 {
+    
+    /**
+     * punto de entrada del proyecto
+     * @param args Argumentos que ingresan por consola
+     */
+    public static void main(String[] args) {
+        /*
+        Curso: Programación Orientada a Objetos     150 hs
+        Días: Lunes Miércoles Viernes   15:00 a 18:30 hs
+        Profe: Carlos Ríos      lawlercarlospatricio@gmail.com
+        Materiales: Google Drive
+        
+        Software: JDK   Java Development Kit
+        
+        Provedores de Java
+        Oracle JDK
+        OpenJDK
+        OpenJ9  eclipse
+        RedHat OpenJDK
+        IBM OpenJDK
+        
+        LTS Long Time Support 8 años
+        
+        Versiones de JDK
+        Version         Liberado            Fin de soporte
+        JDK 8 LTS       Marzo 2014          Marzo 2022
+        JDK 9           Septiembre 2017     Marzo 2018
+        JDK 10          Marzo 2018          Septiembre 2018
+        JDK 11 LTS      Septiembre 2018     Septiembre 2026
+        JDK 12          Marzo 2019          Septiembre 2019
+        JDK 13          Septiembre 2019     Marzo 2020
+        JDK 14          Marzo 2020          Septiembre 2020
+        JDK 15          Septiembre 2020     Marzo 2021
+        JDK 16          Marzo 2021          Septiembre 2021
+        JDK 17 LTS      Septiembre 2021     Septiembre 2029
+        
+        IDEs    Integrated Development Enviroment
+        - Netbeans 11
+        - Eclipse 
+        - IntelliJ
+        - JDeveloper
+        
+        
+        */
+        
+        System.out.println("Hola Mundo!");
+        
+        //Comentarios de una sola linea
+        /* Bloque de comentarios */
+        
+        /**
+         * Comentario JavaDoc
+         * Los comentarios JavaDoc, puede ser vistos desde fuera de un archivo 
+         * binario.
+         * Deben ser colocados delante de la declaración de métodos o de clases.
+         */
+        
+        
+        // Mostrar argumentos de consola
+        
+        // Tipo de datos primitivos de java
+        
+    }
+}
