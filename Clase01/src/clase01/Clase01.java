d8bc31282b390dcc3202910cbd401db4f5946d6c
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-03-21 19:22:17 -0300

    Add files via upload

---
+++ Clase01/src/clase01/Clase01.java
@@ -0,0 +1,16 @@
+/*
+-- 9 ausentes en el curso--
+-- lawlercarlospatricio@gmail.com
+
+ --Actualizar a java 11 y netbeans 11 tambien, ultima version
+(siempre instalar versiones lts que dura el soporte 3 años).  
+*/
+
+package clase01;
+
+public class Clase01 {
+
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ Clase02/src/clase02/Clase02.java
@@ -0,0 +1,38 @@
+package clase02;
+
+public class Clase02 {
+
+    public static void main(String[] args) {
+
+//11/03/20.
+//Literales->Buscar(se utilizan con true o false, no hace falta utilizar la sentencia new).
+//Cada booleano utiliza 1 byte de almacenaje, aunque utilize realmente 1 bit(True or false).
+        /*
+
+Tipos de datos primitivos-->
+boolean(1 byte),
+byte(1 byte),
+short(2 bytes),
+int(4 bytes), el grappet de int en Integer(Los int son objetos de la clase Integer).
+long(8 bytes,para poder ontener un int dentro de este tipo de dato long se debe colocar al final de la literar del long seria "L"-->(long lo=3000000L))
+char(2 bytes,unicode(caracteres), las literales '' representan un caracter, con  "" es un string),
+float(32 bytes,la literal para este tipo de dato es un f al final del valor de la variable float fl=8.85F).
+double(64bytes, posee mayor precision, no se coloca literal por convencion.),
+//var(Se implementa en jdk11 )(no es un tipo de dato como tal,pero la literal que nosotros le asignemos correrespondera al tipo de dato).//
+
+
+
+Tipo de dato NO primitivo-->
+String(el valor del objeto que se guarda en un vector es privdo y se utiliza un metodo publico que me permite el acceso a este objeto vector encapsulado(lenght)).
+
+Metodos de la clase String hay diversos tipo toLowerCase,toUpperCase, etc. que nos ahorran la escritura de programacion
+estructurada(tipo if forech,etc).se usan siempre y cuando tenga el objeto creado.
+
+         */
+
+
+
+
+    }
+
+}
