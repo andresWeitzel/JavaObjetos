890fa57ecababdd5dbfbb5f8ae7f51e44ffed4e7
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-03-27 16:21:51 -0300

    Add files via upload

--- Clase10/src/mypackage/entities/Dato.java
+++ Clase10/src/mypackage/entities/Dato.java
@@ -22,6 +22,53 @@ public class Dato {
         return "Dato{"+"Valor: " + dato + '}';
     }
 
+    
+    /*Utilizaremos redefinicon de metodos para que dos objetos con dos comportamientos diferentes
+    sean considerados iguales*/
+    
+   /* @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.dato;
+        return hash;
+    }*/
+   
+   /* @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Dato other = (Dato) obj;
+        if (this.dato != other.dato) {
+            return false;
+        }
+        return true;
+    }*/
+    
+    
+    //Metodo Resumido?
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Dato)) return false;
+        return this.hashCode()== obj.hashCode();
+    }
+    
+     //Metodo Resumido?
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+
+    
+    
+
    
     
     
